1c3822f1d3964b3086a7f6671cc1ddeac6fc985d
ViAyayay <valgmal@gmail.com>
2020-10-16 01:12:57 +0300

    base

--- desktop/src/ru/geekbrains/game/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import ru.geekbrains.game.StartGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 600;
+		config.width = 800;
 		new LwjglApplication(new StartGame(), config);
 	}
 }
