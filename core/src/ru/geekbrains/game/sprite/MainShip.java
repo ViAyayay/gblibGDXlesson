84ed3293c1da38d882d5e4a5b65ba34a1f0d8636
ViAyayay <valgmal@gmail.com>
2020-11-04 15:38:38 +0300

    add task 7

--- core/src/ru/geekbrains/game/sprite/MainShip.java
+++ core/src/ru/geekbrains/game/sprite/MainShip.java
@@ -15,7 +15,7 @@ public class MainShip extends Ship {
     private static final float SHIP_HEIGHT = 0.15f;
     private static final float MARGIN = 0.05f;
     private static final float RELOAD_INTERVAL = 0.2f;
-    private static final int HP = 10;
+    private static final int HP = 100;
 
     private static final int INVALID_POINTER = -1;
 
